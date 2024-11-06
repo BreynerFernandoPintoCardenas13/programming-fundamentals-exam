c132af9d0816ae31305e05c518f32d5df7e876fb
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-11-06 07:23:40 -0500

    solution exercise 3

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import math
-ladoA=float(input("ingrese el lado A del triangulo: "))
-
-ladoA=((math.sqrt(3))/4*math.pow(ladoA, 2))
-if ladoA<1000:
-    decimal=str(input("quieres el resultado con decimales o enteros?: "))
-    if decimal.lower()=="decimales": 
-        print(f"el area es: {ladoA}")
-    elif decimal.lower()=="enteros":
-        area=round(ladoA)
-        print(f"el area es: {area}")
-    else:
-        print("palabra mal escrita")
+suma=0
+for i in range(1,4):
+    voltajes=float(input(f"enter the diferent voltage {i} : "))
+    suma+=voltajes
+promedio=suma/3
+print(promedio)
+if promedio>220:
+    print("PELIGRO")
+elif promedio>115 and promedio<220:
+    print("ALTO VOLTAJE")
 else:
-    print("DATOS NO VALIDOS")
+    print("VOLTAJE CORRECTO")
