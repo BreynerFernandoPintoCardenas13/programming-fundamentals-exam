d8e7ab297a31700231d73c0ef07581414110f4ea
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-11-06 07:24:21 -0500

    solution exercise 4

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-suma=0
-for i in range(1,4):
-    voltajes=float(input(f"enter the diferent voltage {i} : "))
-    suma+=voltajes
-promedio=suma/3
-print(promedio)
-if promedio>220:
-    print("PELIGRO")
-elif promedio>115 and promedio<220:
-    print("ALTO VOLTAJE")
-else:
-    print("VOLTAJE CORRECTO")
+print("hola como estas")
+distancia=float(input("enter a distancia in metro: "))
+kilometros=distancia/1000
+print(f"{kilometros}km")
